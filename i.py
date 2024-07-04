d0deb071e1604680e94069185961095eb7bc6db7
vastirrr <vasti.rus@stud.ubbcluj.ro>
2024-07-04 17:25:25 +0300

    all good + description

--- i.py
+++ i.py
@@ -6,11 +6,12 @@ import tensorflow as tf
 dataset = pd.read_csv('cancer.csv')
 
 # Separate features (x) and target variable (y)
-x = dataset.drop(columns=["diagnosis(1=m, 0=b)"])
-y = dataset["diagnosis(1=m, 0=b)"]
+x = dataset.drop(columns=["diagnosis(1=m, 0=b)"]) #everything exept this column
+y = dataset["diagnosis(1=m, 0=b)"] #just this column
 
 # Split data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+# test_size=0.2 <=> 20% of the data is going to be tested in set
 
 # Build the neural network model
 model = tf.keras.models.Sequential([
@@ -21,6 +22,7 @@ model = tf.keras.models.Sequential([
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#accuracy bcz we want to clasify as many tumors as possible
 
 # Train the model
 model.fit(x_train, y_train, epochs=1000, batch_size=64, verbose=1)
